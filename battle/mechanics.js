455e5a7e93ace4235b724be8028f9cc0ca7f2989
Tashiro <tashiro-gyori@github.com>
2020-05-05 11:17:14 +0200

    Corrected functions

--- battle/mechanics.js
+++ battle/mechanics.js
@@ -1,5 +1,8 @@
-var turn= 1; // to verify: a turn = "1 action" and not the whole PCs/enemies actions
-var max_turn= 2; // normally 4 because you have 4 PCs except if you have deads... I guess?
+// a turn = "1 action"
+var pc_max_turns= 2;
+var pc_actual_turn= 1;
+var enemy_max_turns=4; // by default
+var enemy_actual_turn=1;
 
 /* todo */
 
@@ -15,16 +18,16 @@ export function calculate_damage() {
 }
 
 export function calculate_speed_enemy(enemy_agility) {
-  if (max_turn==4){
+  if (enemy_max_turns==4){
     return enemy_agility- enemy_agility * 1/6 * turn;
   }
-  if (max_turn==3){
+  if (enemy_max_turns==3){
     return enemy_agility- enemy_agility * 1/4 * turn;
   }
-  if (max_turn==2){
+  if (enemy_max_turns==2){
     return enemy_agility- enemy_agility * 1/2 * turn;
   }
-
+  enemy_actual_turn++;
 }
 
 /* ----------------------------------- */
@@ -32,17 +35,16 @@ export function calculate_speed_enemy(enemy_agility) {
 export function calculate_speed_pc(pc_agility) {
   var rand_number = Math.floor(Math.random() * 65536); // rand number between 0 and 65535
   var speed= pc_agility + ( (pc_agility*rand_number) >> 20 ); // formula: multiply, right shift 20, and add to the basis speed
-  if (turn > 1) // multi-turn penalty: if more than 1, divide by 2
+  if (pc_actual_turn > 1) // multi-turn penalty: if more than 1, divide by 2
     speed= speed / 2;
-  turn++;
   return speed;
 }
 
 
 /* tests */
-var felix_agility=200;
-var piers_agility=148;
-var enemy_agility=120;
+var felix_agility=391;
+var piers_agility=180;
+var enemy_agility=268; // star magician agility
 console.log( calculate_speed_pc(felix_agility) );
 console.log( calculate_speed_pc(piers_agility) );
 console.log( calculate_speed_pc(enemy_agility) );
