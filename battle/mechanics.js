dd96abaab8e79cd3f04b1f566dce0cf32c0b8b2f
Tashiro <35850621+Tashiro-Gyori@users.noreply.github.com>
2020-05-29 10:57:15 +0200

    first version

--- battle/mechanics.js
+++ battle/mechanics.js
@@ -1,36 +1,50 @@
-import { MainChar } from './base/MainChar.js';
+import { MainChar } from '../base/MainChar.js';
+import { Enemy } from '../base/battle/Enemy.js';
+import { Ability } from '../base/Ability.js';
+import { Item } from '../base/Item.js';
 
-var isaac= new MainChar(
-    "isaac",
-    actions,
-    index,
-    walk_speed,
-    dash_speed,
-    climb_speed,
-    push_speed,
-    avatar_image_path,
-    name,
-    hp_curve,
-    pp_curve,
-    atk_curve,
-    def_curve,
-    agi_curve,
-    luk_curve,
-    exp_curve,
-    starting_level,
-    venus_level_base,
-    mercury_level_base,
-    mars_level_base,
-    jupiter_level_base,
-    venus_power_base,
-    mercury_power_base,
-    mars_power_base,
-    jupiter_power_base,
-    venus_resist_base,
-    mercury_resist_base,
-    mars_resist_base,
-    jupiter_resist_base,
-    innate_abilities,
-    in_party,
-    djinni
-);
+
+const ELEMENT_MODIFIER =400;
+const ELEMENT_HALF_MODIFIER =200;
+
+/* ----------- numbers --------------- */
+export function general_random_number(){
+  return Math.floor(Math.random() * 10000 );
+}
+export function battle_random_number(){
+  return Math.floor(Math.random() * 65536 );
+}
+export function random_number(){ // (used in battle)
+  return Math.floor(Math.random() * 4 );
+}
+
+/* ----------- dammage formualas --------------- */
+export function calculate_base_damage(attacker_data, defender_data) {
+  var relative_attack= attacker_data.atk - defender_data.def;
+  return Math.floor(relative_attack / 2);
+}
+
+// this is for djiin OR unleash as they are Elemental Physical Attacks
+// dmg_mult often to 1 (but flint-> 1.6) // extra_dmg depends of the weapons
+export function calculate_physical_damage(attacker_data, defender_data, weapon_data) {
+  var prop1= attacker_data.element_afinity + "_power_current";
+  var prop2= resist_current.element_afinity + "_resist_current";
+  // missing: "weapon_data.dmg_mult" and "weapon_data.extra_dmg"
+  return Math.floor ( (calculate_base_damage(attacker_data, defender_data) * weapon_data.dmg_mult + weapon_data.extra_dmg)* (1+(prop1-prop2)/ELEMENT_MODIFIER) ) + random_number();
+}
+
+// not to be confused with unleash
+//  In battle, Criticals are represented by the camera zooming in on the target, followed by a slightly delayed attack from the user.
+/* CrtRate = 1.25x base, but some weapons have different modifiers set for this.
+Sol Blade = 3x
+Excalibur = 1.25x or 3x
+Tisiphone Edge = 1.25, 2x, or 3x*/
+export function calculate_critical_damage(attacker_data, defender_data, weapon_data) {
+  var extra_dmg= defender_data.level / 5 + 6;
+  return Math.floor ( (calculate_physical_damage(attacker_data, defender_data, weapon_data) * weapon_data.unleash_rate + extra_dmg) + random_number();
+}
+
+export function apply_dammage(target_data){
+  // loop check array length
+  //target.current_hp -= *the corresponding number*;
+}
